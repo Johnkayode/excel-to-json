9bcdafc46b8f5a988c531638e31dce43516c2b34
nerdthejohn <newtonjohn043@gmail.com>
2021-09-27 23:55:22 +0100

    implemented xlsx object to json

--- utils.js
+++ utils.js
@@ -1,4 +1,3 @@
-const { json } = require('express');
 const readexcel = require('xlsx')
 
 const excelFilter = (req, file, cb) => {
@@ -12,11 +11,53 @@ const excelFilter = (req, file, cb) => {
     }
 };
 
+const sheet_to_json = (sheet) => {
+
+  const outputArray = []
+
+ 
+  // Remove unused properties
+  delete sheet['!margins']
+  delete sheet['!ref']
+  delete sheet['!rows']
+
+
+  
+  Object.keys(sheet).forEach(key=>{
+    const [_, column, row] = key.match(/([a-zA-Z]+)(\d+)/)
+
+    if (row !== '1') {
+      const parent = column + '1';
+
+      //We can get the array index we'll put it at by subtracting 2 from the row
+      //Eg A2 => 0, A4 => 2, BB2 => 0
+      const targetRow = parseInt(row) - 2;
+      //Put an empty object in that row if there isn't one already
+      if(!outputArray[targetRow]){
+        outputArray[targetRow] = {}
+      }
+
+      //Then add our item to that row
+      outputArray[targetRow][sheet[parent]['v']] = sheet[key]['v']
+
+    }
+  
+
+    
+  })
+  
+  
+   
+  return outputArray
+
+
+}
+
 const excelTojson = (file) => {
     var workbook = readexcel.read(file.buffer, {type:"buffer"});
     var json_response = []
     for (sheet of workbook.SheetNames){
-      json_response = json_response.concat(readexcel.utils.sheet_to_json(workbook.Sheets[sheet]))
+      json_response = json_response.concat(sheet_to_json(workbook.Sheets[sheet]))
     }
     return json_response
     
